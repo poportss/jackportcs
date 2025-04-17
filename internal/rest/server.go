71e9c81f0e0b6daf8612a7bdad81e6b9c3e14f12
rafaelportela <rafael.portela@pazze.app>
2025-04-17 11:09:32 -0300

    Version 1.0.3 - Adding Readme

--- internal/rest/server.go
+++ internal/rest/server.go
@@ -1,8 +1,11 @@
 package rest
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 // ResponseInternalServerError responds with a StatusInternalServerError, status FAILURE ad errorMessage
@@ -33,4 +36,33 @@ func ResponseForbidden(c *gin.Context) {
 func ResponseNotFound(c *gin.Context) {
 	c.JSON(http.StatusNotFound, gin.H{"status": "FAILURE", "errorMessage": "Not Found"})
 	c.Abort()
+}
+
+func GetMethod(url string) ([]byte, error) {
+	var err error
+
+	httpReq, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	httpReq.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{Timeout: 30 * time.Second}
+
+	res, err := client.Do(httpReq)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer res.Body.Close()
+
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %w", err)
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return nil, fmt.Errorf("error in response: %s", resBody)
+	}
+
+	return resBody, nil
 }
